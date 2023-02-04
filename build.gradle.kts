35bce4732856083b1bda50057a046a720ba38922
mkrol <krol.mariya@gmail.com>
2023-02-05 01:48:35 +0300

    Setup openApiGenerate task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("org.openapi.generator") version "6.3.0"
 }
 
 group = "com.makrol"
@@ -19,4 +20,13 @@ tasks.test {
 
 kotlin {
     jvmToolchain(8)
+}
+
+openApiGenerate {
+    generatorName.set("kotlin")
+    inputSpec.set("$rootDir/src/main/resources/teamCityRestApi-v2018.1-swagger2.0.json")
+    outputDir.set("$buildDir/generated")
+    apiPackage.set("com.makrol.teamcity.api.client.api")
+    invokerPackage.set("com.makrol.teamcity.api.client.invoker")
+    modelPackage.set("com.makrol.teamcity.api.client.model")
 }
