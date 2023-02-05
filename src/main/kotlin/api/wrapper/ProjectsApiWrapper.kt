f3517e0544d2f3e6f24926520aa66ce4ae4b925b
mkrol <krol.mariya@gmail.com>
2023-02-05 21:33:26 +0300

    Add calls for Project API

---
+++ src/main/kotlin/api/wrapper/ProjectsApiWrapper.kt
@@ -0,0 +1,23 @@
+package api.wrapper
+
+import com.makrol.teamcity.api.client.api.ProjectApi
+import com.makrol.teamcity.api.client.model.NewProjectDescription
+import com.makrol.teamcity.api.client.model.Project
+
+class ProjectsApiWrapper : BaseApiClient() {
+    private val projectApi: ProjectApi = ProjectApi(host, baseClient)
+
+    fun createProject(): Project {
+        val newProject = NewProjectDescription(name = "simple_tc_project".appendRandomNumericPostfix())
+
+        return projectApi.addProject(newProject)
+    }
+
+    fun getProject(projectName: String): Project {
+        return projectApi.getProject(projectName)
+    }
+
+    fun deleteProject(project: Project) {
+        projectApi.deleteProject(project.id!!)
+    }
+}
---
+++ src/test/kotlin/tests/CallerCanCreatePipeline.kt
@@ -0,0 +1,27 @@
+package tests
+
+import api.wrapper.ProjectsApiWrapper
+import com.makrol.teamcity.api.client.model.Project
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class CallerCanCreatePipeline {
+    private val projectApi = ProjectsApiWrapper()
+
+    private lateinit var project: Project
+
+    @Test
+    fun createSimpleTeamCityProject() {
+        project = projectApi.createProject()
+        Assertions.assertNotNull(project)
+
+        val createdProject = projectApi.getProject(project.parentProjectName!!)
+        Assertions.assertEquals(project, createdProject)
+    }
+
+    @AfterEach
+    fun deleteProject() {
+        projectApi.deleteProject(project)
+    }
+}
