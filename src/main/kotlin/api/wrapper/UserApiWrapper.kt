a68d12247f198969c3dbfdfdaa15c8b612ee1f70
mkrol <krol.mariya@gmail.com>
2023-02-05 16:57:52 +0300

    Add wrapper for User API

---
+++ src/main/kotlin/api/wrapper/UserApiWrapper.kt
@@ -0,0 +1,27 @@
+package api.wrapper
+
+import com.makrol.teamcity.api.client.api.UserApi
+import com.makrol.teamcity.api.client.model.User
+
+class UserApiWrapper : BaseApiClient() {
+    private val userApi: UserApi = UserApi(host, baseClient)
+
+    fun createUser(): User {
+        val newUser = User(
+            username = "userName".appendRandomNumericPostfix(),
+            name = "name".appendRandomNumericPostfix(),
+            email = "${("fakeMail".appendRandomNumericPostfix())}@example.com",
+            password = "pass".appendRandomNumericPostfix()
+        )
+
+        return userApi.addUser(body = newUser)
+    }
+
+    fun getUser(userName: String): User {
+        return userApi.getUser(userName)
+    }
+
+    fun deleteUser(user: User) {
+        userApi.deleteUser(user.username!!)
+    }
+}
