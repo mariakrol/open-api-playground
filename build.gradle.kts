f1eda27c4d6587c170e0b0c3c1e4ab7f0cb61dad
mkrol <krol.mariya@gmail.com>
2023-02-05 19:43:48 +0300

    Add calls for Ure API

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,20 +12,26 @@ repositories {
     mavenCentral()
 }
 
+val junitVersion = "5.9.2"
 dependencies {
-    testImplementation(kotlin("test"))
-
     //OpenApi client
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+
+    //Tests organization
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${junitVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
+val jvmVersion = 11
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(jvmVersion)
 }
 
 val generatedSourcesPath = "$buildDir/generated"
@@ -42,5 +48,5 @@ kotlin.sourceSets["main"].kotlin.srcDir("$generatedSourcesPath/src/main/kotlin")
 
 tasks.withType<KotlinCompile>().configureEach {
     dependsOn("openApiGenerate")
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = jvmVersion.toString()
 }
---
+++ src/test/kotlin/tests/CallerCanCreateUserByApiTests.kt
@@ -0,0 +1,27 @@
+package tests
+
+import api.wrapper.UserApiWrapper
+import com.makrol.teamcity.api.client.model.User
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class CallerCanCreateUserByApiTests {
+    private val userApi: UserApiWrapper = UserApiWrapper()
+
+    private lateinit var user: User
+
+    @Test
+    fun createUser() {
+        user = userApi.createUser()
+        Assertions.assertNotNull(user)
+
+        val createdUser = userApi.getUser(user.username!!)
+        Assertions.assertEquals(createdUser, user)
+    }
+
+    @AfterEach
+    fun deleteUser() {
+        userApi.deleteUser(user)
+    }
+}
