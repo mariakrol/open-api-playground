d471102bfcc33ae395670d7334589dbe46905c53
mkrol <krol.mariya@gmail.com>
2023-02-09 10:50:24 +0300

    Clean up gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 val junitVersion = "5.9.2"
 dependencies {
-    //OpenApi client
+    //OpenAPI client
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
 
@@ -35,13 +35,16 @@ kotlin {
 }
 
 val generatedSourcesPath = "$buildDir/generated"
+val apiDescriptionFile = getFilePath(fileName = "teamCityRestApi-v2018.1-swagger2.0.json")
+val apiRootName = "com.makrol.teamcity.api.client"
+
 openApiGenerate {
     generatorName.set("kotlin")
-    inputSpec.set("$rootDir/src/main/resources/teamCityRestApi-v2018.1-swagger2.0.json")
+    inputSpec.set(apiDescriptionFile)
     outputDir.set(generatedSourcesPath)
-    apiPackage.set("com.makrol.teamcity.api.client.api")
-    invokerPackage.set("com.makrol.teamcity.api.client.invoker")
-    modelPackage.set("com.makrol.teamcity.api.client.model")
+    apiPackage.set("$apiRootName.api")
+    invokerPackage.set("$apiRootName.invoker")
+    modelPackage.set("$apiRootName.model")
     configOptions.set(mapOf("library" to "jvm-ktor"))
 }
 
@@ -50,4 +53,11 @@ kotlin.sourceSets["main"].kotlin.srcDir("$generatedSourcesPath/src/main/kotlin")
 tasks.withType<KotlinCompile>().configureEach {
     dependsOn("openApiGenerate")
     kotlinOptions.jvmTarget = jvmVersion.toString()
+}
+
+fun getFilePath(baseDir: String = "$rootDir/src", fileName: String): String {
+    return fileTree(baseDir)
+        .filter { it.isFile }.files
+        .first { it.name == fileName }
+        .absolutePath
 }
