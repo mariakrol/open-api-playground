a9e38461e91b25e7b7859ff92980a06d344180b1
mkrol <krol.mariya@gmail.com>
2023-02-09 11:26:27 +0300

    Clean up

--- src/main/kotlin/configuration/ConfigurationProvider.kt
+++ src/main/kotlin/configuration/ConfigurationProvider.kt
@@ -4,6 +4,6 @@ sealed class ConfigurationProvider {
 
     companion object {
         const val teamCityHost = "http://lebkuchenhaus:8111"
-        const val token = "eyJ0eXAiOiAiVENWMiJ9.bHc4TEtQdVBaWnZWWHl1SjNTcHFmTXkycGRB.ZThjOGY3YTAtZmNjMS00MmJhLThlNTktNTUxOGQ2YTliNTc2"
+        const val token = "TOKEN"
     }
 }
