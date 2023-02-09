8a7944a71b95d6666a0d139ef39fe4052bca3161
mkrol <krol.mariya@gmail.com>
2023-02-09 10:46:52 +0300

    Move extension function to the appropriate file

--- src/main/kotlin/api/wrapper/BaseApiClient.kt
+++ src/main/kotlin/api/wrapper/BaseApiClient.kt
@@ -8,15 +8,9 @@ import io.ktor.client.plugins.auth.providers.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.serialization.kotlinx.json.*
-import kotlin.math.abs
-import kotlin.random.Random
 
 abstract class BaseApiClient {
     protected val setupConfig: (HttpClientConfig<*>) -> Unit = { config -> getClientConfig(config) }
-
-    fun String.appendRandomNumericPostfix(separator: String = "_"): String {
-        return "${this}${separator}${abs(Random.nextInt())}"
-    }
 
     private fun getClientConfig(config: HttpClientConfig<*>) {
         config.install(DefaultRequest) {
